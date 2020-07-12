7ce306601c608cedd93c34901855f1feae445c47
chiragdoctor <chirag.doctor@gmail.com>
2020-07-12 12:10:00 +0100

    refactor boiler plate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
-import NameTag from './components/NameTag';
+import React from 'react';
 import './App.scss';
 
-const initialNames = [
-	{ firstName: 'peter', lastName: 'peterson' },
-	{ firstName: 'john', lastName: 'johnson' },
-	{ firstName: 'jill', lastName: 'jillson' },
-	{ firstName: 'john', lastName: 'johnson' },
-];
 function App() {
-	const [names, setNames] = useState(initialNames);
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1 className='title'>Name List</h1>
-				{names.map((v, i) => {
-					return <NameTag key={`${v.firstName}${v.lastName}`} firstName={v.firstName} lastName={v.lastName} />;
-				})}
-			</header>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='title'></h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
