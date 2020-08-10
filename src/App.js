88a7930cd4a144bef8725217116180a8ffcbe1ee
chiragdoctor <chirag.doctor@gmail.com>
2020-08-10 10:28:29 +0100

    make custom hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import usePrevious from './hooks/usePrevious';
 
 function App() {
+  const [age, setAge] = useState(21);
+  const previousAge = usePrevious(age);
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='title'></h1>
+        <h1 className='title'>Use Previous: Custom Hook</h1>
+        <h2>Current Age: {age}</h2>
+        <h2>Previous Age: {previousAge}</h2>
+        <button onClick={() => setAge(age - 1)}>Make me younger</button>
       </header>
     </div>
   );
