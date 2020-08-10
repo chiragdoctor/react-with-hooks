a985968e8446806c6089870dd2a46e4f9157a84d
chiragdoctor <chirag.doctor@gmail.com>
2020-08-10 18:38:37 +0100

    Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
+import AboutPage from './Pages/AboutPage';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='title'></h1>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <Route
+            path='/'
+            exact
+            render={() => {
+              return <h1>Welcome to Home Page</h1>;
+            }}
+          />
+          <Route path='/about' exact component={AboutPage} />
+          <Route
+            path='/user/:firstname/:lastname'
+            exact
+            render={({ match }) => {
+              return (
+                <h1>
+                  {' '}
+                  Welcome {match.params.firstname} {match.params.lastname}
+                </h1>
+              );
+            }}
+          />
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return <div>Welcome to about page.</div>;
+};
+
+export default AboutPage;
