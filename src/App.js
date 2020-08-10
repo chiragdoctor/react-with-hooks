fc75c3f87b75f1687dc019f97463f4caba3a61f0
chiragdoctor <chirag.doctor@gmail.com>
2020-08-10 20:18:02 +0100

    Working with Prompt react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import './App.scss';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect,
+  Prompt,
+} from 'react-router-dom';
+import './App.css';
 import AboutPage from './Pages/AboutPage';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [age, setAge] = useState(0);
+
+  const onClickHandler = () => {
+    setLoggedIn(!loggedIn);
+  };
+
+  const onChangeHandle = (e) => {
+    setAge(e.target.value);
+  };
   return (
     <Router>
       <div className='App'>
         <header className='App-header'>
-          <ul>
-            <li>
+          <ul className='ui-style'>
+            <li className='li-style'>
               <NavLink
                 className='App-link'
                 to='/'
@@ -19,7 +35,7 @@ function App() {
                 Home
               </NavLink>
             </li>
-            <li>
+            <li className='li-style'>
               <NavLink
                 className='App-link'
                 to='/about'
@@ -29,7 +45,8 @@ function App() {
                 About
               </NavLink>
             </li>
-            <li>
+
+            <li className='li-style'>
               <NavLink
                 className='App-link'
                 to='/user/chirag/doctor'
@@ -40,6 +57,17 @@ function App() {
               </NavLink>
             </li>
           </ul>
+          <Prompt
+            when={loggedIn && age <= 0}
+            message={(location) => {
+              return location.pathname.startsWith('/user')
+                ? true
+                : 'Are you sure?';
+            }}
+          ></Prompt>
+          <button className='button' onClick={onClickHandler}>
+            {loggedIn ? 'LOGOUT' : 'LOGIN'}
+          </button>
           <Route
             path='/'
             exact
@@ -52,11 +80,17 @@ function App() {
             path='/user/:firstname/:lastname'
             exact
             render={({ match }) => {
-              return (
-                <h1>
-                  {' '}
-                  Welcome {match.params.firstname} {match.params.lastname}
-                </h1>
+              return loggedIn ? (
+                <div>
+                  <h2>Age : {age}</h2>
+
+                  <input type='text' onChange={onChangeHandle} value={age} />
+                  <h1>
+                    Welcome {match.params.firstname} {match.params.lastname}
+                  </h1>
+                </div>
+              ) : (
+                <Redirect to='/' />
               );
             }}
           />
