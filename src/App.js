e29836fd66cd32a6a750854d6d6be2b85880f5b0
chiragdoctor <chirag.doctor@gmail.com>
2020-08-12 09:28:47 +0100

    mobx - not working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import './App.scss';
+import './App.css';
+import { observer, useObservable } from 'mobx-react-lite';
 
-function App() {
+const App = observer(() => {
+  const store = useObservable({
+    count: 1,
+    addOne() {
+      store.count++;
+    },
+    subtractOne() {
+      store.count--;
+    },
+  });
+
+  function addOneHandle() {
+    store.addOne();
+  }
+
+  function subtractOneHandle() {
+    store.subtractOne();
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='title'></h1>
+        <h1> Count: {store.count}</h1>
+        <button onClick={addOneHandle}> Add One</button>
+        <button onClick={subtractOneHandle}>Subtract One</button>
       </header>
     </div>
   );
-}
+});
 
 export default App;
