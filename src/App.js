0e6397cda477da55384afdfcbdc7aeba1fa706d8
chiragdoctor <chirag.doctor@gmail.com>
2020-08-12 19:13:54 +0100

    Creating a Component Library and Making a generic button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Button from './components/Button/Button';
 
 function App() {
+  const onClickPrimary = () => {
+    alert('Primary button clicked');
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <Button type='primary' onClick={onClickPrimary}>
+          Click Me!!
+        </Button>
+        <Button type='disabled'>Click Me!!</Button>
+      </header>
     </div>
   );
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/common.scss';
+import './Button.scss';
+
+const Button = ({ children, type, onClick = () => {} }) => {
+  let localClass = '';
+
+  if (type === 'primary') {
+    localClass = 'primary';
+  }
+  if (type === 'disabled') {
+    localClass = 'disabled';
+  }
+  const cls = `bgYellow fontBlack pad8 width100 radius15 ${localClass}`.trim();
+  return (
+    <div className='btnClass'>
+      <button onClick={onClick} className={cls}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
